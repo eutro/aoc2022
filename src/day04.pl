b1db43f65fece15fa3e8ffe4206f44903cf53b7f
eutro <benedek.szilvasy@gmail.com>
2022-12-04 05:21:30 +0000

    Day 4

---
+++ src/day04.pl
@@ -0,0 +1,26 @@
+:- use_module(library(dcg/basics)).
+
+main :-
+    phrase_from_stream(parse_input(Input), current_input),
+    count(has_subs, Input, P1),
+    write(P1),nl,!,
+    count(has_intr, Input, P2),
+    write(P2),nl.
+
+count(Goal, Ls, R) :-
+    include(Goal, Ls, Cls),
+    length(Cls, R).
+
+has_subs([Llo, Lhi, Rlo, Rhi]) :- Llo =< Rlo, Rhi =< Lhi.
+has_intr([Llo, Lhi, Rlo, Rhi]) :- Llo =< Rlo, Rlo =< Lhi.
+
+parse_input([]) --> eos.
+parse_input([[Llo, Lhi, Rlo, Rhi] | R]) -->
+    integer(Alo), `-`, integer(Ahi),
+    `,`,
+    integer(Blo), `-`, integer(Bhi),
+    eol, !,
+    { (Alo = Blo -> ([Llo, Rlo] = [Alo, Blo], msort([Ahi, Bhi], [Rhi, Lhi])))
+      ; (Alo < Blo -> [Llo, Lhi, Rlo, Rhi] = [Alo, Ahi, Blo, Bhi])
+      ; [Llo, Lhi, Rlo, Rhi] = [Blo, Bhi, Alo, Ahi] },
+    parse_input(R).
