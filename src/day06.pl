f9487fb3e054fe670a8a6157f3ff5e99aaef03ee
eutro <benedek.szilvasy@gmail.com>
2022-12-06 05:26:38 +0000

    I lied

--- src/day06.pl
+++ src/day06.pl
@@ -3,17 +3,13 @@
 
 main :-
     stream_to_lazy_list(current_input, Line),
-    phrase(marker(4, P1), Line),
-    write(P1),nl,
-    phrase(marker(14, P2), Line),
-    write(P2),nl,
-    true.
+    forall(member(Len, [4, 14]),
+           (phrase(marker(Len, Ans), Line),
+            writeln(Ans))).
 
 marker(Len, I) -->
-    string(Prefix),
     { length(Marker, Len) },
-    Marker,
-    remainder(_),
+    string(Prefix), Marker, remainder(_),
     { sort(Marker, Sorted),
       length(Sorted, Len),
       length(Prefix, I0),
