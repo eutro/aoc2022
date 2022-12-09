29842b53d1717e30789b826496ebd3e04ce021c2
eutro <benedek.szilvasy@gmail.com>
2022-12-09 06:04:06 +0000

    Opts

--- src/day09.pl
+++ src/day09.pl
@@ -7,56 +7,52 @@
 main :-
     phrase_from_stream(insns(Insns), current_input),
     flatten_moves(Insns, Flat),
-    track(Flat, 2, P1),
-    writeln(P1),
-    track(Flat, 10, P2),
-    writeln(P2),
-
-    true.
+    forall(member(Len, [2, 10]),
+           (track(Flat, Len, Ans),
+            writeln(Ans))).
 
 track(Flat, I, Unique) :-
     phrase(run(I, Trail), Flat),
     sort(Trail, Visited),
     length(Visited, Unique).
 
-run(I, [(0, 0) | Trail]) -->
+run(I, Vs) -->
     { length(Rope0, I),
       maplist([_,(0,0)]>>true, Rope0, Rope) },
-    run(I, Trail, Rope).
-run(_, [], _) --> eos.
-run(I, [Tl | Trail], Rope) -->
+    run(I, Rope, [(0, 0)], Vs).
+run(_, _, Vs, Vs) --> eos.
+run(I, Rope, Vs0, Vs) -->
     [mv(Dir)],
-    { move_rope(Rope, Dir, Rope1),
-      nth1(I, Rope1, Tl) },
-    run(I, Trail, Rope1).
+    { move_rope(Rope, Dir, Rope1, Vs0, Vs1) },
+    run(I, Rope1, Vs1, Vs).
 
 mvby((X1, Y1), (DX, DY), (X2, Y2)) :-
     X2 #= X1 + DX,
     Y2 #= Y1 + DY.
 
-move_rope([H | Tl], Dir, [H1 | Tl1]) :-
+move_rope([H | Tl], Dir, [H1 | Tl1], Vs0, Vs1) :-
     move(H, Dir, H1),
-    move_tl(H1, Tl, Tl1).
+    move_tl(H1, Tl, Tl1, Vs0, Vs1).
 
-move_tl(_, [], []) :- !.
-move_tl(H1, [X | Tl], [X | Tl]) :- touching(X, H1), !.
-move_tl(H1, [X | Tl], [X1 | Tl1]) :-
+move_tl(_, [], [], Vs, Vs) :- !.
+move_tl(H1, [X | Tl], [X | Tl], Vs, Vs) :- touching(X, H1), !.
+move_tl(H1, [X], [X1], Vs0, [X1 | Vs0]) :- !, moveto(X, H1, X1).
+move_tl(H1, [X | Tl], [X1 | Tl1], Vs0, Vs) :-
     moveto(X, H1, X1),
-    move_tl(X1, Tl, Tl1).
+    move_tl(X1, Tl, Tl1, Vs0, Vs).
 
-moveto(X, H, X1) :- mvby(X, (0, 2), H), !, mvby(X, (0, 1), X1).
-moveto(X, H, X1) :- mvby(X, (0, -2), H), !, mvby(X, (0, -1), X1).
-moveto(X, H, X1) :- mvby(X, (2, 0), H), !, mvby(X, (1, 0), X1).
-moveto(X, H, X1) :- mvby(X, (-2, 0), H), !, mvby(X, (-1, 0), X1).
+norm(0, 0) :- !.
+norm(X, X1) :- X1 is X div abs(X).
+
 moveto(X, H, X1) :-
-    mvby(X, (DX, DY), H), !,
-    DX1 #= DX div abs(DX),
-    DY1 #= DY div abs(DY),
+    mvby(X, (DX, DY), H),
+    norm(DX, DX1),
+    norm(DY, DY1),
     mvby(X, (DX1, DY1), X1).
 
 touching((X1, Y1), (X2, Y2)) :-
-    abs(X1 - X2) #=< 1,
-    abs(Y1 - Y2) #=< 1.
+    abs(X1 - X2) =< 1,
+    abs(Y1 - Y2) =< 1.
 
 move((X, Y), up, (X, Y1)) :- !, Y1 #= Y - 1.
 move((X, Y), down, (X, Y1)) :- !, Y1 #= Y + 1.
