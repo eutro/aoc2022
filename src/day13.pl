4038ed37877547d5a95e6e2b4b78130348b37cb9
eutro <benedek.szilvasy@gmail.com>
2022-12-13 06:35:13 +0000

    Improve day 13

--- src/day13.pl
+++ src/day13.pl
@@ -6,27 +6,26 @@
 
 main :-
     phrase_from_stream(packets(Packets), current_input),
-    bagof(I, in_order(Packets, I), Is),
-    sum_list(Is, P1),
-    writeln(P1),
-    predsort(cmp, [[[2]], [[6]] | Packets], Sorted),
-    length(Packets, Plen),
-    length(Sorted, Slen),
-    Slen #= Plen + 2,
-    nth1(A, Sorted, [[2]]),
-    nth1(B, Sorted, [[6]]),
-    P2 #= A * B,
-    writeln(P2),
-    true.
+    forall(member(Goal, [phrase(in_order(Ans), Packets),
+                         decoder_key(Packets, Ans)]),
+           (call(Goal), writeln(Ans))).
 
-in_order(Packets, I) :-
-    0 #= I0 mod 2,
-    I1 #= I0 + 1,
-    I #= I0 div 2 + 1,
-    nth0(I0, Packets, Lhs),
-    nth0(I1, Packets, Rhs),
-    cmp(O, Lhs, Rhs),
-    memberchk(O, [(<), (=)]).
+decoder_key(Packets, Key) :-
+    Dividers = [[[2]], [[6]]],
+    append(Dividers, Packets, DPackets),
+    predsort(cmp, DPackets, Sorted),
+    same_length(DPackets, Sorted),
+    maplist(find_packet(Sorted), Dividers, [A, B]),
+    Key #= A * B.
+find_packet(Haystack, Needle, I) :- nth1(I, Haystack, Needle), !.
+
+in_order(Is) --> in_order(1, Is).
+in_order(_, 0) --> eos, !.
+in_order(I, Is) -->
+    [Lhs, Rhs],
+    { (cmp((>), Lhs, Rhs) -> Is = Is0 ; Is #= I + Is0),
+      I1 #= I + 1 },
+    in_order(I1, Is0).
 
 cmp(O, X, Y), (integer(X), integer(Y)) => compare(O, X, Y).
 cmp(O, [], [_|_]) => O = (<).
@@ -39,7 +38,7 @@ cmp(O, X, Y), (list(X), integer(Y)) => cmp(O, X, [Y]).
 list([]).
 list([_|_]).
 
-packets([]) --> eos.
+packets([]) --> eos, !.
 packets([Lhs, Rhs | Tl]) --> packet(Lhs), packet(Rhs), (eol -> packets(Tl) ; Tl = []).
 
 packet(Packet) --> string_without(`\n`, Codes), eol, { string_codes(Str, Codes), term_string(Packet, Str) }.
