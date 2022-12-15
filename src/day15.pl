b13b315cd8a6d6933e263f208620ea8e28e41a3f
eutro <git@eutro.dev>
2022-12-15 06:10:10 +0000

    Day 15

--- src/day15.pl
+++ src/day15.pl
@@ -6,15 +6,31 @@
 
 main :-
     phrase_from_stream(sensors(Sensors), current_input),
-    writeln(Sensors),
-    Row = 2000000,
-    maplist(occludes(Row), Sensors, Spans),
-    collect_spans(Spans, RangeSet),
+    row_count_occluded(Sensors, 2000000, P1),
+    writeln(P1),
+    find_distress(Sensors, 4000000, P2),
+    writeln(P2),
+    true.
+
+find_distress(Sensors, MaxPos, Distress) :-
+    between(0, MaxPos, Row),
+    writeln(Row),
+    row_ranges(Row, Sensors, RangeSet0),
+    subrange(RangeSet0, s(0, MaxPos), RangeSet),
+    X0 #= X - 1,
+    X1 #= X + 1,
+    RangeSet = [s(_, X0), s(X1, _)],
+    Distress is X * 4000000 + Row.
+
+row_count_occluded(Sensors, Row, Occluded) :-
+    row_ranges(Row, Sensors, RangeSet),
     area(RangeSet, Area),
     count_beacons(Sensors, Row, Count),
-    P1 #= Area - Count,
-    writeln(P1),
-    true.
+    Occluded #= Area - Count.
+
+row_ranges(Row, Sensors, RangeSet) :-
+    maplist(occludes(Row), Sensors, Spans),
+    collect_spans(Spans, RangeSet).
 
 count_beacons(Sensors, Row, Count) :-
     maplist(get_dict(beacon), Sensors, Beacons0),
@@ -22,6 +38,16 @@ count_beacons(Sensors, Row, Count) :-
     include(beacon_row(Row), Beacons, Ls),
     length(Ls, Count).
 beacon_row(Row, (_, Row)).
+
+subrange([], _, []) :- !.
+subrange([s(Sf, Ef) | Tl], Range, Rs) :-
+    s(Sr, Er) = Range,
+    (Ef < Sr -> subrange(Tl, Range, Rs)
+    ; Sf > Er -> Rs = []
+    ; S is max(Sf, Sr),
+      E is min(Ef, Er),
+      Rs = [s(S, E) | Tl1],
+      subrange(Tl, Range, Tl1)).
 
 sensors([]) --> eos, !.
 sensors([Sensor | Tl]) --> sensor(Sensor), sensors(Tl).
@@ -32,9 +58,7 @@ area([s(Lo, Hi) | Tl], R) :- R #= (Hi - Lo + 1) + R1, area(Tl, R1).
 collect_spans(Spans, RangeSet) :-
     include(\=(false), Spans, NotFalse),
     sort(NotFalse, Sorted),
-    merge_spans(Sorted, RangeSet),
-    writeln(Sorted),
-    writeln(RangeSet).
+    merge_spans(Sorted, RangeSet).
 
 merge_spans(Spans, Rs) :- merge_spans(Spans, [], Rs).
 merge_spans([], Rs, Rs) :- !.
@@ -42,7 +66,7 @@ merge_spans([Nxt | Spans], [], Rs) :- !, merge_spans(Spans, [Nxt], Rs).
 merge_spans([Nxt | Spans], [Fst | Tl], Rs) :-
     Fst = s(Sf, Ef),
     Nxt = s(Sn, En),
-    (Sn =< Ef
+    (Sn =< Ef + 1
     -> E is max(En, Ef),
        Rs1 = [s(Sf, E) | Tl],
        merge_spans(Spans, Rs1, Rs)
