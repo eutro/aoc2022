6e36900bb1c9571ed12f74cca00836fa6a021a1d
eutro <git@eutro.dev>
2022-12-19 12:40:11 +0000

    Day 19

---
+++ src/day19.c
@@ -0,0 +1,140 @@
+#include <SWI-Prolog.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define MAT_C 4
+#define INGR_C 3
+#define GEODE 3
+
+typedef struct { unsigned res[MAT_C]; } resources_t;
+typedef resources_t pat_t;
+typedef struct { unsigned counts[MAT_C]; } robots_t;
+typedef struct { pat_t pats[MAT_C]; } blueprint_t;
+
+static void recur(
+  blueprint_t *bp,
+
+  int time,
+  resources_t resources,
+  robots_t robots,
+
+  unsigned *max_geodes
+) {
+  bool could_build = false;
+  for (unsigned robot = 0; robot < MAT_C; ++robot) { // which robot to build
+    unsigned time_to_wait = 0;
+    pat_t pat = bp->pats[robot];
+    for (unsigned ingr = 0; ingr < INGR_C; ++ingr) { // limiting ingredient
+      if (resources.res[ingr] >= pat.res[ingr]) continue;
+      unsigned production = robots.counts[ingr];
+      if (production == 0) goto cantbuild;
+      unsigned required = pat.res[ingr] - resources.res[ingr];
+      unsigned time_to_produce = (required / production) + (required % production != 0);
+      if (time_to_produce > time_to_wait) time_to_wait = time_to_produce;
+    }
+
+    int new_time = time - (int) time_to_wait - 1;
+    if (new_time <= 0) continue;
+
+    robots_t new_robots = robots;
+    new_robots.counts[robot]++;
+    resources_t new_resources;
+    for (unsigned i = 0; i < MAT_C; ++i) {
+      new_resources.res[i]
+        = resources.res[i]
+        + robots.counts[i] * (time_to_wait + 1)
+        - pat.res[i];
+    }
+
+    // if we built a geode robot every turn from now, could we beat the current max?
+    if ((((new_time - 1) * new_time) / 2)
+        + new_resources.res[GEODE]
+        + new_time * new_robots.counts[GEODE]
+        < *max_geodes) {
+      continue;
+    }
+
+    could_build = true;
+    recur(
+      bp,
+
+      new_time,
+      new_resources,
+      new_robots,
+
+      max_geodes
+    );
+
+  cantbuild:;
+    continue;
+  }
+
+  if (!could_build) {
+    unsigned geodes
+      = resources.res[GEODE]
+      + robots.counts[GEODE] * (unsigned) time;
+    // printf("have %u, robots %u, at %i -> %u\n",
+    //        resources.res[GEODE],
+    //        robots.counts[GEODE],
+    //        time,
+    //        geodes);
+    if (geodes > *max_geodes) *max_geodes = geodes;
+  }
+}
+
+unsigned simulate_bp(
+  blueprint_t *bp,
+  int time
+) {
+  unsigned max_geodes = 0;
+  recur(
+    bp,
+    time,
+    (resources_t) {{ 0, 0, 0, 0 }},
+    (robots_t) {{ 1, 0, 0, 0 }},
+    &max_geodes
+  );
+  return max_geodes;
+}
+
+foreign_t pl_simulate_bp(
+  term_t t_time,
+  term_t t_blueprint,
+  term_t t_res
+) {
+  uint64_t time;
+  term_t t_pats = PL_new_term_ref();
+  if (!PL_get_uint64_ex(t_time, &time) ||
+      !PL_get_arg(1, t_blueprint, t_pats)) PL_fail;
+
+  atom_t keys[] = {
+    PL_new_atom("ore"),
+    PL_new_atom("clay"),
+    PL_new_atom("obsidian"),
+    PL_new_atom("geode"),
+  };
+
+  blueprint_t blueprint;
+  for (unsigned i = 0; i < MAT_C; i++) {
+    term_t t_pat = PL_new_term_refs(5);
+    if (!PL_get_arg(i + 1, t_pats, t_pat)) PL_fail;
+    for (unsigned j = 0; j < MAT_C; j++) {
+      term_t t_mat = t_pat + j + 1;
+      uint64_t mat;
+      if (!PL_get_dict_key(keys[j], t_pat, t_mat) ||
+          !PL_get_uint64_ex(t_mat, &mat)) PL_fail;
+      blueprint.pats[i].res[j] = (unsigned) mat;
+    }
+    PL_reset_term_refs(t_pat);
+  }
+
+  uint64_t res = simulate_bp(&blueprint, (int) time);
+  if (!PL_unify_uint64(t_res, res)) PL_fail;
+
+  PL_succeed;
+}
+
+int main(int argc, char **argv) {
+  PL_register_foreign("simulate_bp", 3, pl_simulate_bp, 0);
+  return !PL_initialise(argc, argv);
+}
---
+++ src/day19.pl
@@ -0,0 +1,47 @@
+:- use_module(library(dcg/basics)).
+:- use_module(library(clpfd)).
+:- use_module(util).
+
+main :-
+    phrase_from_stream(blueprints(Blueprints), current_input),
+    foreach((var(Ans), member(Mode, [p1, p2])),
+            (simulate(Mode, Blueprints, Ans),
+             writeln(Ans))).
+
+simulate(p1, Bps, Ans) :-
+    maplist(simulate_bp(24), Bps, Geodes),
+    maplist([bp(_, Id), G, R]>>(R is Id * G), Bps, Geodes, Qualities),
+    sum(Qualities, #=, Ans).
+
+simulate(p2, Bps0, Ans) :-
+    length(Bps, 3),
+    append(Bps, _, Bps0),
+    maplist(simulate_bp(32), Bps, Geodes),
+    foldl([A, B, C]>>(C is A * B), Geodes, 1, Ans).
+
+blueprints([]) --> blanks, eos, !.
+blueprints([Blueprint | Tl]) --> blueprint(Blueprint), blueprints(Tl).
+
+blueprint(bp(ps(Ore, Clay, Obsidian, Geode), Id)) -->
+    blanks, `Blueprint `, integer(Id), `:`, !,
+    pattern(ore, Ore),
+    pattern(clay, Clay),
+    pattern(obsidian, Obsidian),
+    pattern(geode, Geode).
+
+empty_pat(p{ore: 0, clay: 0, obsidian: 0, geode: 0}).
+is_pat(p{ore: _, clay: _, obsidian: _, geode: _}).
+
+pattern(Mat, Deps) -->
+    blanks, `Each `, !, mat(Mat), ` robot costs `, deps(Deps).
+
+deps(M) -->
+    integer(Cost), ` `, mat(Mat),
+    (`.` -> { R = p{ore: 0, clay: 0, obsidian: 0, geode: 0} }
+    ; ` and `, deps(R)),
+    { M = R.put(Mat, Cost) }.
+
+mat(ore) --> `ore`, !.
+mat(clay) --> `clay`, !.
+mat(obsidian) --> `obsidian`, !.
+mat(geode) --> `geode`, !.
