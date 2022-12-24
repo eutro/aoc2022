5edca43b123d2285a835de1717b43b2eb505af5b
eutro <git@eutro.dev>
2022-12-24 01:14:39 +0000

    Oops I lapsed into C again

---
+++ src/day23.c
@@ -0,0 +1,235 @@
+#include <SWI-Prolog.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TABLE_BREADTH 1024
+#define TABLE_DEPTH 16
+#define MAX_PROPOS 2048
+#define DIR_COUNT 4
+
+typedef struct { int32_t x, y; } pos_t;
+
+typedef struct {
+  pos_t entries[TABLE_DEPTH];
+  size_t len;
+} table_row_t;
+typedef struct {
+  table_row_t rows[TABLE_BREADTH];
+} grid_t;
+typedef struct {
+  grid_t grid;
+  size_t elfc;
+  pos_t *elves;
+} state_t;
+typedef struct {
+  pos_t to;
+  size_t elf;
+} proposition_t;
+
+static size_t hash_pos(pos_t pos) { return (size_t) pos.x * 1123 + (size_t) pos.y; }
+#define TABLE_ROW(GRID, POS) &grid->rows[hash_pos(pos) % TABLE_BREADTH];
+static bool lookup_pos(grid_t *grid, pos_t pos) {
+  table_row_t *row = TABLE_ROW(grid, pos);
+  for (size_t i = 0; i < row->len; ++i) {
+    pos_t found = row->entries[i];
+    if (found.x == pos.x && found.y == pos.y) return true;
+  }
+  return false;
+}
+static void add_pos(grid_t *grid, pos_t pos) {
+  table_row_t *row = TABLE_ROW(grid, pos);
+  if (row->len == TABLE_DEPTH) {
+    fprintf(stderr, "Max hash collisions exceeded\n");
+    exit(1);
+  }
+  row->entries[row->len++] = pos;
+}
+static void remove_pos(grid_t *grid, pos_t pos) {
+  table_row_t *row = TABLE_ROW(grid, pos);
+  for (size_t i = 0; i < row->len; ++i) {
+    pos_t found = row->entries[i];
+    if (found.x == pos.x && found.y == pos.y) {
+      if (i != row->len - 1) {
+        row->entries[i] = row->entries[row->len - 1];
+      }
+      row->len--;
+      return;
+    }
+  }
+}
+
+static int cmp_propo(const void *lhs_v, const void *rhs_v) {
+  const proposition_t *lhs = lhs_v, *rhs = rhs_v;
+  if (lhs->to.x < rhs->to.x) return -1;
+  if (lhs->to.x > rhs->to.x) return 1;
+  if (lhs->to.y < rhs->to.y) return -1;
+  if (lhs->to.y > rhs->to.y) return 1;
+  return 0;
+}
+
+static const pos_t directions[4][3] = {
+  {{ 0, -1}, {-1, -1}, { 1, -1}},
+  {{ 0,  1}, {-1,  1}, { 1,  1}},
+  {{-1,  0}, {-1, -1}, {-1,  1}},
+  {{ 1,  0}, { 1, -1}, { 1,  1}},
+};
+
+/*
+static void print_state(state_t *state) {
+  int
+    min_x = INT_MAX,
+    max_x = INT_MIN,
+    min_y = INT_MAX,
+    max_y = INT_MIN;
+  for (size_t elf = 0; elf < state->elfc; ++elf) {
+    pos_t pos = state->elves[elf];
+    if (pos.x < min_x) min_x = pos.x;
+    if (pos.x > max_x) max_x = pos.x;
+    if (pos.y < min_y) min_y = pos.y;
+    if (pos.y > max_y) max_y = pos.y;
+  }
+
+  puts("Grid:");
+  for (int y = min_y; y <= max_y; ++y) {
+    for (int x = min_x; x <= max_x; ++x) {
+      char sym = lookup_pos(&state->grid, (pos_t) {x, y}) ? '#' : '.';
+      putchar(sym);
+    }
+    puts("");
+  }
+}
+*/
+
+static void step_state(state_t *state, size_t *counter, size_t until) {
+  proposition_t propos[MAX_PROPOS];
+  size_t propoc;
+  for (; *counter < until; ++*counter) {
+    // print_state(state);
+    propoc = 0;
+    for (size_t elf = 0; elf < state->elfc; ++elf) {
+      pos_t pos = state->elves[elf];
+      for (int dx = -1; dx <= 1; ++dx) {
+        for (int dy = -1; dy <= 1; ++dy) {
+          if (dx == 0 && dy == 0) continue;
+          pos_t nb_pos = { pos.x + dx, pos.y + dy };
+          if (lookup_pos(&state->grid, nb_pos)) {
+            goto has_nb;
+          }
+        }
+      }
+      continue;
+
+    has_nb:;
+      unsigned move_dir;
+      for (unsigned ddir = 0; ddir < DIR_COUNT; ++ddir) {
+        unsigned dir = (*counter + ddir) % DIR_COUNT;
+        for (unsigned i = 0; i < 3; ++i) {
+          pos_t dpos = directions[dir][i];
+          pos_t nb_pos = { pos.x + dpos.x, pos.y + dpos.y };
+          if (lookup_pos(&state->grid, nb_pos)) {
+            goto cant_move;
+          }
+        }
+        move_dir = dir;
+        goto has_move;
+      cant_move:;
+        continue;
+      }
+      continue;
+    has_move:;
+      pos_t dpos = directions[move_dir][0];
+      pos_t nb_pos = { pos.x + dpos.x, pos.y + dpos.y };
+      proposition_t propo = { nb_pos, elf };
+      propos[propoc++] = propo;
+    }
+
+    qsort(propos, propoc, sizeof(*propos), cmp_propo);
+    size_t dst = 0;
+    for (size_t src = 0; src < propoc; ++src) {
+      pos_t to = propos[src++].to;
+      bool add = true;
+      for (; src < propoc; ++src) {
+        pos_t to_o = propos[src].to;
+        if (to.x != to_o.x || to.y != to_o.y) break;
+        add = false;
+      }
+      src--;
+      if (add) {
+        propos[dst++] = propos[src];
+      }
+    }
+
+    if (dst == 0) {
+      ++*counter;
+      return;
+    }
+
+    for (size_t pr = 0; pr < dst; ++pr) {
+      proposition_t propo = propos[pr];
+      pos_t *elf = &state->elves[propo.elf];
+      remove_pos(&state->grid, *elf);
+      *elf = propo.to;
+      add_pos(&state->grid, *elf);
+    }
+  }
+}
+
+static foreign_t pl_step_state(
+  term_t t_posns0,
+  term_t t_counter0,
+  term_t t_limit,
+  term_t t_posns,
+  term_t t_counter
+) {
+  uint64_t limit, counterv;
+  if (!PL_get_uint64_ex(t_limit, &limit) ||
+      !PL_get_uint64_ex(t_counter0, &counterv)) PL_fail;
+  size_t counter = (size_t) counterv;
+
+  state_t state; memset(&state, 0, sizeof(state));
+
+  functor_t f_posns;
+  if (!PL_get_functor(t_posns0, &f_posns)) PL_fail;
+  state.elfc = PL_functor_arity(f_posns);
+  pos_t elves[state.elfc];
+  state.elves = elves;
+
+  term_t
+    t_posn = PL_new_term_ref(),
+    t_pos_coord = PL_new_term_ref(),
+    t_pos_coord1 = PL_new_term_ref();
+  for (size_t elf = 0; elf < state.elfc; ++elf) {
+    int64_t x_i, y_i;
+    if (!PL_get_arg(elf + 1, t_posns0, t_posn) ||
+        !PL_get_arg(1, t_posn, t_pos_coord) ||
+        !PL_get_int64_ex(t_pos_coord, &x_i) ||
+        !PL_get_arg(2, t_posn, t_pos_coord) ||
+        !PL_get_int64_ex(t_pos_coord, &y_i)) PL_fail;
+    pos_t pos = { (int) x_i, (int) y_i };
+    elves[elf] = pos;
+    add_pos(&state.grid, pos);
+  }
+
+  step_state(&state, &counter, (size_t) limit);
+
+  if (!PL_unify_functor(t_posns, f_posns)) PL_fail;
+  functor_t f_pair = PL_new_functor(PL_new_atom(","), 2);
+  for (size_t elf = 0; elf < state.elfc; ++elf) {
+    pos_t pos = state.elves[elf];
+    if (!PL_put_int64(t_pos_coord, pos.x) ||
+        !PL_put_int64(t_pos_coord1, pos.y) ||
+        !PL_cons_functor(t_posn, f_pair, t_pos_coord, t_pos_coord1) ||
+        !PL_unify_arg(elf + 1, t_posns, t_posn)) PL_fail;
+  }
+
+  if (!PL_unify_uint64(t_counter, (uint64_t) counter)) PL_fail;
+  PL_succeed;
+}
+
+int main(int argc, char **argv) {
+  PL_register_foreign("step_state", 5, pl_step_state, 0);
+  return !PL_initialise(argc, argv);
+}
--- src/day23.pl
+++ src/day23.pl
@@ -3,93 +3,20 @@
 :- use_module(util).
 
 main :-
-    phrase_from_stream(grid(Grid), current_input),
-    % write_grid(Grid),
-    steptimes(10, 0-Grid, S1-Grid1),
+    phrase_from_stream(grid(Grid0), current_input),
+    step_state(Grid0, 0, 10, Grid1, 10),
     count_empty(Grid1, P1),
     writeln(P1),
-    stepfix(10, P2, S1-Grid1, _),
-    writeln(P2),
-    true.
+    step_state(Grid1, 10, 9999, _, P2),
+    writeln(P2).
 
 count_empty(Grid, Empty) :-
+    functor(Grid, _, Filled),
     grid_extrema(Grid, Lo, Hi),
-    findall(Pos,
-            (inrange((Lo, Hi), Pos),
-             \+ rb_lookup(Pos, _, Grid)),
-            Posns),
-    length(Posns, Empty).
+    boundslen((Lo, Hi), All),
+    Empty is All - Filled.
 
-write_grid_posns(Grid) :-
-    rb_keys(Grid, Keys),
-    writeln(Keys).
-write_grid(Grid) :-
-    write_grid_posns(Grid),
-    grid_extrema(Grid, (X1, Y1), (X2, Y2)),
-    forall(between(Y1, Y2, Y),
-           (forall(between(X1, X2, X),
-                   ((rb_lookup((X, Y), _, Grid)
-                    -> Vl = '#'
-                    ; Vl = '.'),
-                    write(Vl))),
-            nl)).
-
-steptimes(0, G, G) :- !.
-steptimes(N, S0-Grid0, S-Grid) :-
-    N1 is N - 1,
-    step(S0-Grid0, S1-Grid1, _),
-    % write_grid(Grid1),
-    steptimes(N1, S1-Grid1, S-Grid).
-
-stepfix(N0, N, S0-Grid0, S-Grid) :-
-    N1 is N0 + 1,
-    step(S0-Grid0, S1-Grid1, Moved),
-    (Moved -> stepfix(N1, N, S1-Grid1, S-Grid)
-    ; N = N1).
-
-step(S0-Grid0, S-Grid, Moved) :-
-    collect_proposals(S0-Grid0, Proposals0),
-    reduce_proposals(Proposals0, Proposals),
-    (Proposals = [] -> Moved = false ; Moved = true),
-    apply_proposals(Grid0, Proposals, Grid),
-    S is (S0 + 1) mod 4.
-
-collect_proposals(S-Grid, Proposals) :-
-    findall(Proposal, proposal(S-Grid, Proposal), Proposals).
-proposal(S-Grid, Proposal) :-
-    rb_in(Pos, _, Grid),
-    once((between(-1, 1, DX),
-          between(-1, 1, DY),
-          once((DX \= 0 ; DY \= 0)),
-          mvby(Pos, (DX, DY), Nb),
-          rb_lookup(Nb, _, Grid))),
-    once((between(0, 3, CDir),
-          Consider is (S + CDir) mod 4,
-          \+ (consider_dir(Consider, Delta),
-              mvby(Pos, Delta, CPos),
-              rb_lookup(CPos, _, Grid)),
-          consider_delta(Consider, Delta),
-          mvby(Pos, Delta, Target),
-          Proposal = Target-Pos)).
-
-consider_dir(0, D) => member(D, [( 0, -1), (-1, -1), ( 1, -1)]).
-consider_dir(1, D) => member(D, [( 0,  1), (-1,  1), ( 1,  1)]).
-consider_dir(2, D) => member(D, [(-1,  0), (-1, -1), (-1,  1)]).
-consider_dir(3, D) => member(D, [( 1,  0), ( 1, -1), ( 1,  1)]).
-consider_delta(Dir, D) :- once(consider_dir(Dir, D)).
-
-reduce_proposals(P0, P) :- keysort(P0, P1), phrase(rprop(P), P1).
-rprop([]) --> eos, !.
-rprop(P) --> [K-V], (consume_props(K) -> { P = Tl } ; { P = [K-V | Tl] }), rprop(Tl).
-consume_props(K) --> [K-_], (consume_props(K) -> [] ; []).
-
-apply_proposals(Grid0, Proposals, Grid) :- foldl(apply_proposal, Proposals, Grid0, Grid).
-:- det(apply_proposal/3).
-apply_proposal(To-From, Grid0, Grid) :-
-    rb_delete(Grid0, From, Grid1),
-    rb_insert(Grid1, To, [], Grid).
-
-grid_extrema(Grid, Lo, Hi) :- rb_keys(Grid, Posns), extrema(Posns, Lo, Hi).
+grid_extrema(Grid, Lo, Hi) :- Grid =.. [_ | Posns], extrema(Posns, Lo, Hi).
 extrema(Posns, Lo, Hi) :- phrase(extrema(Lo, Hi), Posns).
 extrema(L, H) --> [Pos], extrema(Pos, Pos, L, H).
 extrema(L, H, L, H) --> eos, !.
@@ -99,9 +26,9 @@ extrema((Lx0, Ly0), (Hx0, Hy0), Lo, Hi) -->
       Hx is max(Hx0, X), Hy is max(Hy0, Y) },
     extrema((Lx, Ly), (Hx, Hy), Lo, Hi).
 
-grid(Grid) --> grid0(1, Grid0), { list_to_rbtree(Grid0, Grid) }.
+grid(Grid) --> grid0(1, Grid0), { Grid =.. [g | Grid0] }.
 grid0(_, []) --> eos, !.
 grid0(Y, Row) --> row(1, Y, Tl, Row), { Y1 is Y + 1 }, grid0(Y1, Tl).
 row(_, _, Tl, Tl) --> eol, !.
 row(X, Y, Tl, Row) --> `.`, !, { X1 is X + 1 }, row(X1, Y, Tl, Row).
-row(X, Y, Tl, [(X, Y)-[] | Row]) --> `#`, { X1 is X + 1 }, row(X1, Y, Tl, Row).
+row(X, Y, Tl, [(X, Y) | Row]) --> `#`, { X1 is X + 1 }, row(X1, Y, Tl, Row).
