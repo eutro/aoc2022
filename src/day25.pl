1fb48f4becb7a25c593aac216b961cfa555a2ca3
eutro <git@eutro.dev>
2022-12-25 11:14:26 +0000

    Day 25

---
+++ src/day25.pl
@@ -0,0 +1,30 @@
+:- use_module(library(dcg/basics)).
+:- use_module(library(clpfd)).
+:- use_module(util).
+
+main :-
+    phrase_from_stream(snafus(Snafus), current_input),
+    maplist(snafu_number, Snafus, Numbers),
+    sum(Numbers, #=, Sum),
+    snafu_number(SnafuSum, Sum),
+    string_codes(Ans, SnafuSum),
+    writeln(Ans).
+
+snafu_number(Snafu, Number) :-
+    reverse(Snafu, Rsnafu),
+    rsnafu_number(Rsnafu, Number), !.
+rsnafu_number([], 0).
+rsnafu_number([Digit | Tl], N) :-
+    D #= N mod 5,
+    snafu_digit(D, Digit),
+    N1 #= (N + 2) div 5,
+    rsnafu_number(Tl, N1).
+
+snafu_digit(0, 0'0).
+snafu_digit(1, 0'1).
+snafu_digit(2, 0'2).
+snafu_digit(3, 0'=).
+snafu_digit(4, 0'-).
+
+snafus([]) --> eos, !.
+snafus([Snafu | Tl]) --> string_without(`\n`, Snafu), eol, snafus(Tl).
